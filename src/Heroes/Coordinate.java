fa2fd0434f76544e92d4b758bd90fd3e1345449f
Anna <anasoft@yandex.ru>
2024-03-12 21:06:41 +0500

    homework 2

---
+++ src/Heroes/Coordinate.java
@@ -0,0 +1,13 @@
+public class Coordinate {
+    int x;
+    int y;
+
+    public Coordinate(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double calculateDistance(Coordinate other) {
+        return Math.sqrt(Math.pow(this.x - other.x, 2) + Math.pow(this.y - other.y, 2));
+    }
+}
